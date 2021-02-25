07da42e7f96f3d7a62d88e01758a0004bf5c4971
RomanThinks <61527005+RomanThinks@users.noreply.github.com>
2021-02-24 23:29:25 -0800

    Fibonacci code by Roman

---
+++ Fibonacci.py
@@ -0,0 +1,23 @@
+# Roman Rodriguez, Joshua DeLosSantos, Arturo Verdugo
+# Team Saber
+# CSE 5408
+# Spring 2021
+# Lab 4
+# e.) Fibonnaci sums
+
+counter = 0
+
+print('input number to give fibonacci sum:')
+
+string = input()
+
+userNumber = int(string)
+
+#begin summing the fibonnaci, the sum of previous to the next sum
+
+for counter in userNumber+1:
+    #counter starts at zero
+    
+    
+    counter = counter + 1
+    
