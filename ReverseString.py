438e999b71f4412909d51f464416889909d9cbc3
RomanThinks <61527005+RomanThinks@users.noreply.github.com>
2021-02-24 18:27:59 -0800

    Python code to reverse string inputted by user

---
+++ ReverseString.py
@@ -0,0 +1,19 @@
+# Roman Rodriguez, Joshua DeLosSantos, Arturo Verdugo
+# Team Saber
+# CSE 5408
+# Spring 2021
+# Lab 4
+# a.) reverse a user inputted string
+
+toReverse = ''
+
+print('input a string to be reversed')
+
+toReverse = input()
+
+print(''.join(reversed(toReverse))) #reverse the whole string and printed
+
+                                    #''.join allows for the reversed text to be displayed
+                                    #going over, '<reversed object at (some hex)>' message
+
+
